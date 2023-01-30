a26397bfc5fe43b8b26d0a10a96d84d4d7dc3bd9
shoxruxshomurodov <shomurodovshoxrux1998@gmail.com>
2023-01-30 15:46:45 +0500

    fix:unusedImport

--- components/blocks/food-services/index.ts
+++ components/blocks/food-services/index.ts
@@ -1,5 +1,4 @@
 import HeroSection from "./hero-section";
 import MenuSection from "./menu-section";
-import MealOption from "./meal-option";
 
-export { MenuSection, MealOption, HeroSection };
+export { MenuSection, HeroSection };
