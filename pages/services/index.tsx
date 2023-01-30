bf74e7865cef43341e5d87b1d72984aed06a83fd
shoxruxshomurodov <shomurodovshoxrux1998@gmail.com>
2023-01-30 15:50:36 +0500

    fix:unusedImport

--- pages/services/index.tsx
+++ pages/services/index.tsx
@@ -1,21 +1,8 @@
-import Image from "next/image";
-import React, { useEffect, useRef, useState } from "react";
-import { Banner, Modal, PassengerFilter, Tab } from "../../components";
-import { MainLayout } from "../../layouts";
-import clsx from "clsx";
-import { isNull } from "lodash";
-import { foodOptions, foodsMenu } from "../../mock/foods";
-import {
-  HeroSection,
-  MealOption,
-  MenuSection,
-} from "../../components/blocks/food-services";
+import React from "react";
 import { ServicesContainer } from "../../containers";
 
 const Index = () => {
-  return (
-    <ServicesContainer />
-  );
+  return <ServicesContainer />;
 };
 
 export default Index;
