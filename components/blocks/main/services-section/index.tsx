5d53f5ac8f29dd6c8d3bd1ec4b357803078dc354
shoxruxshomurodov <shomurodovshoxrux1998@gmail.com>
2023-02-03 13:21:26 +0500

    fix:styles

--- components/blocks/main/services-section/index.tsx
+++ components/blocks/main/services-section/index.tsx
@@ -22,234 +22,242 @@ import { coopiratives } from "../../../../mock/coopiratives";
 import { Container } from "../../../../UI";
 import Card from "../../../card";
 import Heading from "../../../heading";
-import { SeatVideo } from "../../../../assets/videos";
 
 const Index = () => {
   return (
     <Fragment>
-      <Container className={"my-[120px]"}>
+      <Container className={"mt-[120px] px-[15px] md:px-8 lg:px-[60px]"}>
         <Heading
           title="Bizning xizmatlar"
           subTitle="Engage active clients at the right time and save time chasing unqualified leads"
           titleClass="text-[32px] leading-[45px] md:text-[45px] mb-2"
         />
-        <div className="grid grid-cols-12">
+        <div className="grid grid-cols-12 gap-6">
           <Card className="col-span-12 md:col-span-6 lg:col-span-5 flex flex-col justify-between p-9 bg-[#F9EFF1] relative group">
-            <Heading
-              title="O‘rindiqni tanlash"
-              subTitle="Engage active clients at the right time and save time chasing"
-              titleClass="text-[20px] lg:text-[32px]"
-              subTitleClass="text-sm md:text-base max-w-sm"
-              className="mb-2.5 lg:mb-5"
-            />
             <div>
-              <ArrowButton
-                onClick={() => {
-                  console.log("Working");
-                }}
+              <Heading
+                title="O‘rindiqni tanlash"
+                subTitle="Engage active clients at the right time and save time chasing"
+                titleClass="text-[20px] lg:text-[32px]"
+                subTitleClass="text-sm md:text-base max-w-sm"
+                className="mb-2.5 lg:mb-5"
               />
+              <div>
+                <ArrowButton
+                  onClick={() => {
+                    console.log("Working");
+                  }}
+                />
+              </div>
             </div>
             <div className="relative h-64 lg:h-96 xl:h-[26rem]">
               <div className="absolute -bottom-9 z-10">
-                <Image
-                  src={sofaImage}
-                  alt="sofa image"
-                  className="z-[10000] "
-                />
+                <Image src={sofaImage} alt="sofa image" />
               </div>
-              <div className="absolute top-24 lg:top-28 right-1/4 w-[170px] xl:w-52 ">
+              <div className="absolute top-24 lg:top-28 right-1/4 w-[170px] lg:w-52 ">
                 <Image src={bgSofa} alt="sofa image" />
               </div>
             </div>
-            <div className="absolute hidden group-hover:block w-[250px] h-[250px] right-9  overflow-hidden">
-              <div className="relative">
-                {/* <ReactPlayer
-                  url={"https://www.youtube.com/watch?v=CTmKrwFu7wg"}
-                  width={250}
-                  playing
-                  loop
-                  muted={true}
-                  height={250}
-                  className="react-player border absolute overflow-hidden top-0 left-0 rounded-full"
-                /> */}
-                <video
-                  controls
-                  className="video"
-                  src={"../../../../assets/videos/seat-video.mp4"}
-                ></video>
-              </div>
+            <div className="video-player  w-[150px]  lg:w-[200px] xl:w-[250px] h-[150px] lg:h-[200px]  xl:h-[250px] absolute right-9 z-10 top-20 md:top-32 hidden group-hover:block">
+              <ReactPlayer
+                url={
+                  "https://www.youtube.com/watch?v=41H5xEI5QmE?controls=0&loop=1?autoplay=1"
+                }
+                playing
+                loop
+                autoPlay
+                muted={true}
+                className="video-circle bg-slate-500"
+              />
             </div>
-            {/* <Heading
-              title="O‘rindiqni tanlash"
-              subTitle="Engage active clients at the right time and save time chasing"
-              titleClass="text-[20px] lg:text-[32px]"
-              subTitleClass="text-sm md:text-base max-w-sm"
-              className="mb-2.5 lg:mb-5 px-9 pt-9"
-            />
-            <div className="px-9">
-              <ArrowButton />
-            </div> */}
-            {/* <div className="relative h-64 lg:h-96 xl:h-[26rem]">
-              <div className="z-50 absolute bottom-0">
-                <Image
-                  src={sofaImage}
-                  alt="sofa image"
-                  className="z-[10000] "
-                />
-              </div>
-              <div className="-z-10 absolute top-20 lg:top-28 right-1/4 w-[155px] xl:w-52">
-                <Image src={bgSofa} alt="sofa image" />
-              </div>
-            </div> */}
           </Card>
-        </div>
-      </Container>
-      <section className="mt-[120px] container mx-auto">
-        <div className="container mx-auto grid grid-cols-12 gap-6 mb-28">
-          <Card className="col-span-12 md:col-span-6 lg:col-span-7 flex flex-col justify-between -z-20">
-            <Heading
-              title="Mazzali taomnoma"
-              subTitle="Engage active clients at the right time and save time chasing unqualified leads"
-              titleClass="text-[20px] lg:text-[32px]"
-              subTitleClass="text-sm md:text-base max-w-sm"
-              className="mb-2.5 lg:mb-5 px-9 pt-9"
-            />
-            <div className="px-9">
-              <ArrowButton />
+          <Card className="col-span-12 md:col-span-6 lg:col-span-7 flex flex-col justify-between p-9 bg-[#F9EFF1] relative group">
+            <div>
+              <Heading
+                title="O‘rindiqni tanlash"
+                subTitle="Engage active clients at the right time and save time chasing"
+                titleClass="text-[20px] lg:text-[32px]"
+                subTitleClass="text-sm md:text-base max-w-sm"
+                className="mb-2.5 lg:mb-5"
+              />
+              <div>
+                <ArrowButton
+                  onClick={() => {
+                    console.log("Working");
+                  }}
+                />
+              </div>
             </div>
-            <div className="grid grid-cols-12 h-64 lg:h-96 xl:h-[26rem] relative">
+            <div className="grid grid-cols-12 h-64 lg:h-96 xl:h-[26rem] relative -mx-9 -mb-9">
               <Image
                 src={bgMeal}
                 alt="sofa image"
-                className="absolute top-1/2  left-1/2 -translate-y-1/2 -translate-x-1/2 w-48 lg:w-64 -z-10"
+                className="absolute top-1/2  left-1/2 -translate-y-1/2 -translate-x-1/2 w-48 lg:w-64"
               />
-              <div className="col-span-12 justify-self-center ">
+              <div className="col-span-12 justify-self-center z-10">
                 <Image
                   src={mealImage2}
                   alt="sofa image"
-                  className="w-20 lg:w-36"
+                  className="w-20 lg:w-36 z-10"
                 />
               </div>
-              <div className="col-span-6 align-self-end relative">
+              <div className="col-span-6 align-self-end relative z-10">
                 <Image
                   src={mealImage1}
                   alt="sofa image"
                   layout="fill"
-                  className="rounded-bl-[25px] w-full"
+                  className="rounded-bl-[25px] !w-9/12 z-10 "
                 />
               </div>
-              <div className="col-span-5 justify-self-center align-self-center">
+              <div className="col-span-5 justify-self-center align-self-center z-10">
                 <Image
                   src={mealImage3}
                   alt="sofa image"
-                  className="w-28 lg:w-52"
+                  className="w-28 lg:w-52 z-10"
                 />
               </div>
+            </div>
+            <div className="video-player  w-[150px]  lg:w-[200px] xl:w-[250px] h-[150px] lg:h-[200px]  xl:h-[250px] absolute right-9 z-10 top-20 md:top-32 hidden group-hover:block">
+              <ReactPlayer
+                url={
+                  "https://www.youtube.com/watch?v=41H5xEI5QmE?controls=0&loop=1?autoplay=1"
+                }
+                playing
+                loop
+                autoPlay
+                muted={true}
+                className="video-circle bg-slate-500"
+              />
             </div>
           </Card>
-          <Card className="col-span-12 md:col-span-6 lg:col-span-7  flex flex-col justify-between -z-20">
-            <Heading
-              title="Yuklaringiz xavfsizligi"
-              subTitle="Engage active clients at the right time and save time chasing unqualified leads"
-              titleClass="text-[20px] lg:text-[32px]"
-              subTitleClass="text-sm md:text-base max-w-sm"
-              className="mb-2.5 lg:mb-5 px-9 pt-9"
-            />
-            <div className="px-9">
-              <ArrowButton />
-            </div>
-            <div className="relative h-64 lg:h-96 xl:h-[26rem]">
-              <div className="z-50 absolute bottom-0">
-                <Image
-                  src={caseImage}
-                  alt="sofa image"
-                  className="z-[10000] "
+          <Card className="col-span-12 md:col-span-6 lg:col-span-7  flex flex-col justify-between p-9 bg-[#F9EFF1] relative group">
+            <div>
+              <Heading
+                title="Yuklaringiz xavfsizligi"
+                subTitle="Engage active clients at the right time and save time chasing unqualified leads"
+                titleClass="text-[20px] lg:text-[32px]"
+                subTitleClass="text-sm md:text-base max-w-sm"
+                className="mb-2.5 lg:mb-5"
+              />
+              <div>
+                <ArrowButton
+                  onClick={() => {
+                    console.log("Working");
+                  }}
                 />
               </div>
-              <div className="-z-10 absolute top-1/2 -translate-y-1/2">
+            </div>
+            <div className="relative h-64 lg:h-96 xl:h-[26rem]  -mx-9 -mb-9">
+              <div className="z-50 absolute bottom-0">
+                <Image src={caseImage} alt="sofa image" />
+              </div>
+              <div className="absolute top-1/2 -translate-y-1/2">
                 <Image src={bgCase} alt="sofa image" />
               </div>
             </div>
-          </Card>
-          <Card className="col-span-12 md:col-span-6 lg:col-span-5 flex flex-col justify-between -z-20">
-            <Heading
-              title="Fast treck, CIP, VIP"
-              subTitle="Engage active clients at the right time and save time chasing unqualified leads"
-              titleClass="text-[20px] md:text-[32px]"
-              subTitleClass="text-sm md:text-base max-w-sm"
-              className="mb-2.5 md:mb-5 px-9 pt-9"
-            />
-            <div className="px-9">
-              <ArrowButton />
+            <div className="video-player  w-[150px]  lg:w-[200px] xl:w-[250px] h-[150px] lg:h-[200px]  xl:h-[250px] absolute right-9 z-10 top-20 md:top-32 hidden group-hover:block">
+              <ReactPlayer
+                url={
+                  "https://www.youtube.com/watch?v=41H5xEI5QmE?controls=0&loop=1?autoplay=1"
+                }
+                playing
+                loop
+                autoPlay
+                muted={true}
+                className="video-circle bg-slate-500"
+              />
             </div>
-            <div className="relative h-64 lg:h-96 xl:h-[26rem]">
+          </Card>
+          <Card className="col-span-12 md:col-span-6 lg:col-span-5  flex flex-col justify-between p-9 bg-[#F9EFF1] relative group">
+            <div>
+              <Heading
+                title="Fast treck, CIP, VIP"
+                subTitle="Engage active clients at the right time and save time chasing unqualified leads"
+                titleClass="text-[20px] lg:text-[32px]"
+                subTitleClass="text-sm md:text-base max-w-sm"
+                className="mb-2.5 lg:mb-5"
+              />
+              <div>
+                <ArrowButton
+                  onClick={() => {
+                    console.log("Working");
+                  }}
+                />
+              </div>
+            </div>
+            <div className="relative h-64 lg:h-96 xl:h-[26rem]  -mx-9 -mb-9">
               <div className="z-50 absolute bottom-0">
                 <Image
                   src={carImage}
                   alt="sofa image"
-                  className="z-[10000] rounded-bl-[20px]"
+                  className="rounded-bl-[20px]"
                 />
               </div>
-              <div className="-z-10 absolute top-1/3 -translate-y-1/2">
+              <div className="absolute top-1/3 -translate-y-1/2 w-1/2 md:w-full">
                 <Image src={bgCar} alt="sofa image" />
               </div>
             </div>
+            <div className="video-player  w-[150px]  lg:w-[200px] xl:w-[250px] h-[150px] lg:h-[200px]  xl:h-[250px] absolute right-9 z-10 top-20 md:top-32 hidden group-hover:block">
+              <ReactPlayer
+                url={
+                  "https://www.youtube.com/watch?v=41H5xEI5QmE?controls=0&loop=1?autoplay=1"
+                }
+                playing
+                loop
+                autoPlay
+                muted={true}
+                className="video-circle bg-slate-500"
+              />
+            </div>
           </Card>
         </div>
-        <div className="relative md:mx-0  px-[15px] md:px-8 xl:px-[60px] ">
-          <div className="min-h-[350px] md:min-h-[400px] xl:min-h-[620px] mx-[-15px] md:-mx-8 xl:-mx-[60px] my-28  flex items-center text-white font-montserrat">
-            <Image
-              src={bgServices}
-              fill
-              alt="Background Image"
-              className="-z-50"
-            />
-            <div className="container mx-auto grid grid-cols-12  px-[15px] md:px-8 xl:px-[60px]">
-              <div className="col-span-12 md:col-span-6 xl:col-span-6">
-                <h2 className="max-w-2xl text-[45px] font-bold leading-[60px] mb-2 hidden lg:block">
-                  Aviachipta sotib olgandan keyingi xizmatlar
-                </h2>
-                <h3 className="lg:hidden text-[32px] leading-[45px] font-bold">
-                  Bizning xizmatlar
+      </Container>
+      <div className="relative mt-24 mb-96 md:mb-64">
+        <Image
+          src={bgServices}
+          fill
+          objectFit="cover"
+          alt="Background Image"
+          className="-z-50"
+          quality={100}
+        />
+        <Container className="pt-[126px] lg:pt-[250px] pb-[70px] lg:pb-[86px] px-[15px] md:px-8 lg:px-[60px]">
+          <div className="flex flex-col md:flex-row justify-between text-white">
+            <div className="max max-w-2xl">
+              <h2 className="text-[32px] md:text-[45px] leading-[45px] md:leading-[60px] font-montserrat font-bold mb-[1px] md:mb-2 line-clamp-1 md:line-clamp-4">
+                Aviachipta sotib olgandan keyingi xizmatlar
+              </h2>
+              <p className="text-[16px] leading-[30px] font-inter font-normal mb-2.5 md:mb-8 line-clamp-2 md:line-clamp-4">
+                Effortless time tracking and reporting. Save time and money with
+                the time tracker trusted by more than 10,000 organizations.
+              </p>
+              <Link href={"/services"}>
+                <button className="bg-primary-red px-16 py-4 rounded-[10px] text-[22px] font-medium text-white font-inter mt-2.5">
+                  Izlash
+                </button>
+              </Link>
+            </div>
+            <div className="w-full lg:max-w-xl relative  mt-7 xl:mt-20">
+              <div className="bg-[#F9EFF1] w-full lg:max-w-[530px] absolute top-0 p-8 xl:p-[50px]  rounded-[30px]">
+                <h3 className="text-[20px] leading-[30px] font-semibold font-inter max-w-[200px] text-[#202020] mb-8 md:mb-14">
+                  Biz bilan hamkor avia kompaniyalar
                 </h3>
-                <p className="text-base font-inter font-normal hidden lg:block max-w-2xl xl:mb-8">
-                  Effortless time tracking and reporting. Save time and money
-                  with the time tracker trusted by more than 10,000
-                  organizations.
-                </p>
-                <p className="lg:hidden">
-                  Engage active clients at the right time and save time chasing
-                  unqualified leads
-                </p>
-                <Link href={"/services"}>
-                  <button className="bg-primary-red px-16 py-4 rounded-[10px] text-[22px] font-medium text-white font-inter mt-2.5">
-                    Izlash
-                  </button>
-                </Link>
-              </div>
-              <div className="hidden xl:block xl:col-span-1 "></div>
-              <div className="col-span-12 md:col-span-6 xl:col-span-5 relative mt-8 md:mt-16">
-                <div className="absolute bg-[#F9EFF1] p-8 rounded-[25px] w-full text-black lg:-bottom-full xl:translate-y-1/5">
-                  <h6 className="text-sm md:text-base lg:text-xl font-inter font-semibold leading-5 max-w-[150px] md:max-w-[200px]">
-                    Biz bilan hamkor avia kompaniyalar
-                  </h6>
-                  <div className="grid grid-cols-12 gap-2 lg:gap-8 align-middle">
-                    {map(coopiratives, (cooperative) => (
-                      <div className="col-span-4 flex items-center">
-                        <Image
-                          src={get(cooperative, "image")}
-                          alt="Logo"
-                          layout="responsive"
-                        />
-                      </div>
-                    ))}
-                  </div>
+                <div className="grid grid-cols-12 gap-2 lg:gap-8 align-middle mb-2 md:mb-10">
+                  {map(coopiratives, (cooperative) => (
+                    <div className="col-span-4 flex items-center">
+                      <Image
+                        src={get(cooperative, "image")}
+                        alt="Logo"
+                        layout="responsive"
+                      />
+                    </div>
+                  ))}
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
+        </Container>
+      </div>
     </Fragment>
   );
 };
