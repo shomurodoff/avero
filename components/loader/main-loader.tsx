512772186ab31405737cf009bb158da241683af4
shoxruxshomurodov <shomurodovshoxrux1998@gmail.com>
2023-02-14 18:38:59 +0500

    feat:main-loader

--- components/loader/main-loader.tsx
+++ components/loader/main-loader.tsx
@@ -7,6 +7,7 @@ import VideoLoader from "./video-loader";
 
 const MainLoader = () => {
   const [percentage, setPercentage] = useState<number>(0);
+  const [play, setPlay] = useState<boolean>(false);
   const upTo = percentage < 100;
 
   useEffect(() => {
@@ -15,29 +16,37 @@ const MainLoader = () => {
     return () => clearInterval(intervalId);
   }, [upTo]);
 
-  if (isEqual(percentage, 100)) {
-    return <VideoLoader />;
-  }
-
+  useEffect(() => {
+    setPlay(true);
+  }, []);
   return (
-    <div className="fixed inset-0 bg-primary-blue">
-      <div className="flex flex-col justify-between h-full">
-        <div>
-          <div className="py-10 w-full flex justify-center">
-            <Image src={MainPageLogo} alt="Logo Image" />
+    <div>
+      <div
+        className={clsx(
+          !isEqual(percentage, 100) ? "fixed inset-0 bg-primary-blue" : "hidden"
+        )}
+      >
+        <div className="flex flex-col justify-between h-full">
+          <div>
+            <div className="py-10 w-full flex justify-center">
+              <Image src={MainPageLogo} alt="Logo Image" />
+            </div>
+            <div
+              className={clsx("h-1 bg-primary-red mt-5")}
+              style={{
+                width: `${percentage}%`,
+              }}
+            ></div>
           </div>
-          <div
-            className={clsx("h-1 bg-primary-red mt-5")}
-            style={{
-              width: `${percentage}%`,
-            }}
-          ></div>
+          <div className="text-right">
+            <h1 className="font-inter font-normal text-[130px] leading-[158px] xl:text-[300px] xl:leading-[363px] text-white">
+              {percentage} %
+            </h1>
+          </div>
         </div>
-        <div className="text-right">
-          <h1 className="font-inter font-normal text-[130px] leading-[158px] xl:text-[300px] xl:leading-[363px] text-white">
-            {percentage} %
-          </h1>
-        </div>
+      </div>
+      <div className={clsx(isEqual(percentage, 100) ? "block" : "hidden")}>
+        <VideoLoader play={play} />
       </div>
     </div>
   );
--- components/loader/video-loader.tsx
+++ components/loader/video-loader.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 
-const VideoLoader = () => {
+interface Props {
+  play: boolean;
+}
+
+const VideoLoader: React.FC<Props> = ({ play }) => {
   return (
     <div className="absolute top-0 left-0 w-full h-full overflow-y-hidden bg-slate-400">
-      <video autoPlay muted loop className="min-w-full min-h-full object-cover">
+      <video
+        autoPlay={play}
+        muted
+        loop
+        className="min-w-full min-h-full object-cover"
+      >
         <source src="video-loader.mp4" type="video/mp4" />
       </video>
     </div>
