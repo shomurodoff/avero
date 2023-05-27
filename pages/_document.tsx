c30254fb609a3ce89b6718f0b3fa764414b29ca2
BoburUsmanoff <bobur@pixeel.co.uk>
2023-05-27 11:07:55 +0500

    fix:metadata

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,25 +1,27 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import {Html, Head, Main, NextScript} from "next/document";
 import React, {Fragment} from "react";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-        <Head>
-            <title>Avero | Home</title>
-            <meta property="title" content="Avero.uz"  />
-            <meta name="description" content="Avero.uz"/>
-            <meta property="og:url" content="http://avero.uz/" />
-            <meta property="og:description" content="Avero.uz"/>
-            <meta property="og:image" content="/logo.png" />
-
-            <meta name="viewport" content="width=device-width, initial-scale=1"/>
-            <link rel="icon" href="/favicon.ico"/>
-        </Head>
-      <body>
-        <Main />
-        <div id="modal" />
-        <NextScript />
-      </body>
-    </Html>
-  );
+    return (
+        <Html lang="en">
+            <Head>
+                <meta name="keywords" content="website, pdp, university, it"/>
+                <meta name="description" content="PDP University - O'zbekistondagi to'laqonli IT universitet."/>
+                <meta property="title" content="PDP University"/>
+                <meta itemProp="image" content="/logo.png"/>
+                <meta property="og:title" content="PDP University"/>
+                <meta property="og:image" content="/logo.png"/>
+                <meta property="og:description" content="PDP University - O'zbekistondagi to'laqonli IT universitet."/>
+                <meta property="og:url" content="https://university.pdp.uz"/>
+                <meta property="og:site_name" content="PDP University"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+            <body>
+            <Main/>
+            <div id="modal"/>
+            <NextScript/>
+            </body>
+        </Html>
+    );
 }
