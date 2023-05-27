7afed0166f641c05f3e383c8b1b60db97d14d39c
BoburUsmanoff <bobur@pixeel.co.uk>
2023-05-27 10:48:50 +0500

    fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,15 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import React, {Fragment} from "react";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+        <Head>
+            <title>Avero | Home</title>
+            <meta name="description" content="Avero.uz"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <link rel="icon" href="/favicon.ico"/>
+        </Head>
       <body>
         <Main />
         <div id="modal" />
